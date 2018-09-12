984e71ebf62a69d99c749be8ef61fab135b767f1
akembrar7795 <akembrar7795@gmail.com>
2018-09-12 14:02:15 -0400

    trail_model

--- Models/TrailModels/TrailCreate.cs
+++ Models/TrailModels/TrailCreate.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,21 @@ namespace Models.TrailModels
 {
     public class TrailCreate
     {
+        [Required]
+        public int TrailID { get; set; }
+        [Required]
+        public Guid OwnerID { get; set; }
+        [Required]
+        public string TrailName { get; set; }
+        [Required]
+        public decimal TrailDistance { get; set; }
+        [Required]
+        public Difficulty TrailDifficulty { get; set; }
+        [Required]
+        public bool IsOpen { get; set; }
+        [Required]
+        public int ParkID { get; set; }
+
+        public virtual Park Park { get; set; }
     }
 }
