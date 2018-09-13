c9b807e5580aefab5fe0588eeee240f4009602cf
Austen Gren <austenbgren@gmail.com>
2018-09-13 16:11:49 -0400

    add my park CRUD create detail edit listitem

--- Models/MyParkModels/MyParkEdit.cs
+++ Models/MyParkModels/MyParkEdit.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,25 @@ namespace Models.MyParkModels
 {
     public class MyParkEdit
     {
+        [Required]
+        [Display(Name = "Park Name")]
+        public int ParkID { get; set; }
+        public string ParkName { get; set; }
+        public virtual Park Park { get; set; }
+
+        [Required]
+        [Display(Name = "Trail Name")]
+        public int TrailID { get; set; }
+        public string TrailName { get; set; }
+        public virtual Trail Trail { get; set; }
+
+        [Required]
+        public MyTrailStatus MyTrailStatus { get; set; }
+
+        [Required]
+        [Display(Name = "Trail Comment")]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(1000, ErrorMessage = "There are too many characters in this field.")]
+        public string TrailComment { get; set; }
     }
 }
--- Models/MyParkModels/MyParkListItem.cs
+++ Models/MyParkModels/MyParkListItem.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,26 @@ namespace Models.MyParkModels
 {
     public class MyParkListItem
     {
+        [Required]
+        [Display(Name = "Park Name")]
+        public int ParkID { get; set; }
+        public string ParkName { get; set; }
+        public virtual Park Park { get; set; }
+
+        [Required]
+        [Display(Name = "Trail Name")]
+        public int TrailID { get; set; }
+        public string TrailName { get; set; }
+        public virtual Trail Trail { get; set; }
+
+        [Required]
+        [Display(Name = "My Trail Status")]
+        public MyTrailStatus MyTrailStatus { get; set; }
+
+        [Required]
+        [Display(Name = "Trail Comment")]
+        public string TrailComment { get; set; }
+
+        public override string ToString() => TrailName;
     }
 }
